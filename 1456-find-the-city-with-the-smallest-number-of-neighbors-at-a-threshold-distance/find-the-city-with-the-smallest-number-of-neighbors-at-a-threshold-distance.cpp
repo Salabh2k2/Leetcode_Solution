28e281d76938763f768483b8c5c1c38b476f79d5
Salabh Rai <102615559+Salabh2k2@users.noreply.github.com>
2024-01-31 20:10:55 +0530

    Time: 15 ms (99.13%) | Memory: 15.7 MB (24.66%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> cost(n, vector<int>(n, 1e9));
+        
+        // Initialize cost matrix
+        for(auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            cost[u][v] = wt;
+            cost[v][u] = wt; // Since it's an undirected graph
+        }
+        
+        for(int i = 0; i < n; i++) {
+            cost[i][i] = 0;
+        }
+        
+        // Floyd-Warshall Algorithm
+        for(int k = 0; k < n; k++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
+                }
+            }
+        }
+        
+        map<int,int> reachableCities;
+        for(int i = 0; i < n; i++) {
+            int reachableCount = 0;
+            for(int j = 0; j < n; j++) {
+                if(i != j && cost[i][j] <= distanceThreshold) {
+                    reachableCount++;
+                }
+            }
+            reachableCities[i] = reachableCount;
+        }
+        
+        int minReachableCount = n, minCity = -1;
+        for(auto it : reachableCities) {
+            if(it.second <= minReachableCount) {
+                minReachableCount = it.second;
+                minCity = it.first;
+            }
+        }
+        
+        return minCity;
+    }
+};
