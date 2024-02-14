fb3c377011cc35272d8f0d1c2fce869ced7fbc90
Salabh Rai <102615559+Salabh2k2@users.noreply.github.com>
2024-02-14 22:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (39.52%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    int dp[100001];
-    vector<int>chosen;
-    int rec(int level, int n, vector<int>nums)
-    {
-        if(level<0) return 0;
-        
-        int ans = 0;
-        //if i choose
-        if(dp[level]!=-1) return dp[level];
-        ans = max(ans,rec(level-2,n,nums)+nums[level]);
-        // if i donot choose
-         ans = max(ans,rec(level-1,n,nums));
-        return dp[level] = ans;
-        
-    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
+        int n  = nums.size();
+        int dp[n+1];
         memset(dp,-1,sizeof(dp));
-        return rec(n-1,n,nums);
-        
+        dp[0]= 0;
+        dp[1] = nums[0];
+        for(int i = 2;i<=n;i++)
+        {
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i-1]);
+        }
+        return dp[n];
     }
 };
