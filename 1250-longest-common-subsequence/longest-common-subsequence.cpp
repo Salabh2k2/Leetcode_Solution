d7904d2856ca5a0e5989cefe4660b80c5224407c
Salabh Rai <102615559+Salabh2k2@users.noreply.github.com>
2024-03-04 20:31:27 +0530

    Time: 42 ms (19.93%) | Memory: 25.9 MB (6.89%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.length();
-        int n = text2.length();
-        vector<vector<int>> memo(m + 1, vector<int>(n + 1, -1));
-        return lcsMemo(text1, text2, m, n, memo);
+    int rec(int level1, int level2, string &text1, string &text2, vector<vector<int>>&dp)
+    {
+        if(level1<0 || level2<0) return 0 ;
+
+        if(dp[level1][level2]!=-1) return dp[level1][level2];
+
+        if(text1[level1]==text2[level2])
+        {
+            return dp[level1][level2] = 1+ rec(level1-1, level2-1, text1, text2, dp);
+        }
+
+        return dp[level1][level2] = max(rec(level1-1,level2,text1,text2,dp), rec(level1,level2-1,text1,text2,dp));
+
     }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
 
-private:
-    int lcsMemo(string& text1, string& text2, int m, int n, vector<vector<int>>& memo) {
-        if (m == 0 || n == 0)
-            return 0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
 
-        if (memo[m][n] != -1)
-            return memo[m][n];
-
-        if (text1[m - 1] == text2[n - 1])
-            memo[m][n] = 1 + lcsMemo(text1, text2, m - 1, n - 1, memo);
-        else
-            memo[m][n] = max(lcsMemo(text1, text2, m - 1, n, memo), lcsMemo(text1, text2, m, n - 1, memo));
-
-        return memo[m][n];
+        return (rec(n-1,m-1,text1,text2,dp));
     }
 };
