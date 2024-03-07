0ec7d1d5cbc7a467d233f180504790574bf1f192
Salabh Rai <102615559+Salabh2k2@users.noreply.github.com>
2024-03-07 19:30:57 +0530

    Time: 31 ms (12.51%) | Memory: 11.5 MB (20.85%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
+
+    vector<int> printingLongestIncreasingSubsequence(vector<int> arr, int n) {
+    if (n == 0) return {}; // Handle the case when the input array is empty
+    vector<int> dp(n, 1);
+    vector<int> hash(n, -1); // Correct initialization
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i] && dp[j] + 1 > dp[i] && arr[i]%arr[j]==0) {
+                dp[i] = 1 + dp[j];
+                hash[i] = j;
+            }
+        }
+    }
+
+    int ans = -1;
+    int lastIndex = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (dp[i] > ans) {
+            ans = dp[i];
+            lastIndex = i;
+        }
+    }
+
+    vector<int> temp;
+    while (lastIndex != -1) {
+        temp.push_back(arr[lastIndex]);
+        lastIndex = hash[lastIndex];
+    }
+    reverse(temp.begin(), temp.end());
+    return temp;
+}
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> groupSize(n, 1), prevElement(n, -1);
-        int maxIndex = 0;
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] % nums[j] == 0 && groupSize[i] < groupSize[j] + 1) {
-                    groupSize[i] = groupSize[j] + 1;
-                    prevElement[i] = j;
-                }
-            }
-            if (groupSize[i] > groupSize[maxIndex]) {
-                maxIndex = i;
-            }
-        }
-
-        vector<int> result;
-        for (int i = maxIndex; i != -1; i = prevElement[i]) {
-            result.insert(result.begin(), nums[i]);
-        }
-        return result;
+        sort(nums.begin(),nums.end());
+        vector<int> temp = printingLongestIncreasingSubsequence(nums,n);
+        return temp;
     }
 };
