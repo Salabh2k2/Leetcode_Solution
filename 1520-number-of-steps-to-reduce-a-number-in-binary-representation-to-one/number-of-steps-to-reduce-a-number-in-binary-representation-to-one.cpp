078317ae8323ca6fec1ee90a8487af26e7dd5329
Salabh Rai <102615559+Salabh2k2@users.noreply.github.com>
2024-05-29 11:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (15.86%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int n = s.size();
+        map<int, vector<int>> mp;
+        int ans = 0;
+
+        // Populate the map with positions of '0's
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0') {
+                mp[0].push_back(i);
+            }
+        }
+
+        int j = n - 1;
+        while (j >= 0) {
+            // Remove elements from mp[0] that are not relevant anymore
+            while (!mp[0].empty() && mp[0].back() >= j) {
+                mp[0].pop_back();
+            }
+            
+            if (s[j] == '0') {
+                j--;
+                ans++;
+            } else {
+                vector<int>& temp = mp[0];
+                int k = temp.size();
+                if (k == 0) {
+                    if(j==0) break;
+                    ans += (j + 2);
+                    break;
+                }
+                int ind = temp.back();
+                s[ind] = '1';
+                s[j] = '0';
+                ans++;
+                temp.pop_back();
+            }
+        }
+        return ans;
+    }
+};
