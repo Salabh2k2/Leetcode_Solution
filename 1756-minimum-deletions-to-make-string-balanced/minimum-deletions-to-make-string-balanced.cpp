932f06578425000812908468af28dd6c533028d5
Salabh Rai <102615559+Salabh2k2@users.noreply.github.com>
2024-07-30 15:44:39 +0530

    Time: 43 ms (99.04%) | Memory: 49.5 MB (33.17%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    int minimumDeletions(string s) {
-        int a_count=0;
-        int min_del=0;
-
-        for(char ch : s){
-            if(ch=='b'){
-                a_count++;
-            }
-            else{
-                min_del=min(min_del+1,a_count);
-            }
+    int minimumDeletions(string& s) {
+        const int n=s.size();
+        vector<int> B_pre(n), A_suf(n);//0-based prefix sums & suffix sums
+        B_pre[0]=s[0]=='b';
+        A_suf[0]=count(s.begin(), s.end(), 'a');// Count 'a' in s
+        for(int i=1; i<n; i++){
+            B_pre[i]=B_pre[i-1]+(s[i]=='b');
+            A_suf[i]=A_suf[i-1]-(s[i-1]=='a');
         }
-        return min_del;
+        int cnt=n;
+        for(int i=0; i<n; i++){
+            cnt=min(cnt, B_pre[i]+A_suf[i]-1);
+        }
+        return cnt;
     }
 };
 
-/*
-class Solution {
-public:
-    int minimumDeletions(string s) {
-        int n = s.size();
-        vector<int>suffix(n,0);
-        suffix[n-1] = (s[n-1]=='a');
-        for(int i = n-2;i>=0;i--)
-        {
-            if(s[i]=='a') suffix[i] = suffix[i+1]+1;
-            else suffix[i] = suffix[i+1];
-        }
-        vector<int>prefix(n,0);
-        prefix[0] = (s[0]=='b');
-        for(int i = 1;i<n;i++)
-        {
-            if(s[i]=='b') prefix[i] = prefix[i-1]+1;
-            else prefix[i] = prefix[i-1];
-        }
-        int ans = 0;
-        for(int i  = n-1;i>=0;i--)
-        {
-            ans = 
-        }
 
-        return ans;
-    }
-};*/
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
